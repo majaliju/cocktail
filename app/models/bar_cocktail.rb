f5f1f3305b70136b3dee42ae02e02e53ee57ae6c
majaliju <majalijunyc@gmail.com>
2023-03-05 17:18:36 -0500

    removed initialize method

--- app/models/bar_cocktail.rb
+++ app/models/bar_cocktail.rb
@@ -2,23 +2,25 @@ class BarCocktail < ApplicationRecord
   belongs_to :bar
   belongs_to :cocktail_drink
 
-# ##! need to review the intialize method 
-#   @@total_count = []
+##! need to review the intialize method 
+  
 
-#   ## on initialization 
-#   def initialize
-#       @@total_count << self
-#   end
+  # ## on initialization 
+  # def initialize(total_count)
+  #     @@total_count = []
+  #     total_count = @@total_count
+  #     total_count << self
+  # end
 
 
-#   def self.how_many
-#     @@total_count
-#   end
-
-  def average_rating
-    ## take all reviews associated with this instance
-    ## get the average rating (a few ways to do it)
+  def self.how_many
+    total_count
   end
+
+  # def average_rating
+  #   ## take all reviews associated with this instance
+  #   ## get the average rating (a few ways to do it)
+  # end
 
 
   ###! need to remember how to find the associated method without explicitly spelling it out
