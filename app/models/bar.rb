f505858fa61a6e30fa2f55eefd5c3aa664a9230b
majaliju <majalijunyc@gmail.com>
2023-03-05 17:31:23 -0500

    few model updates; testing out tally methods

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,7 +1,19 @@
 class Bar < ApplicationRecord
   has_many :bar_cocktails
-  # has_many :reviews, through: :bar_cocktails
   has_many :cocktail_drinks, through: :bar_cocktails
+  has_many :reviews, through: :bar_cocktails
+
+  after_create :update_count
 
   ## average rankings
+  @@total_count = 0
+  def update_count
+    @@total_count += 1
+  end
+
+  def self.count
+    @@total_count
+  end
+
+
 end
