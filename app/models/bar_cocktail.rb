21e99e635edeefd69060d2257da4b57376bbb176
majaliju <majalijunyc@gmail.com>
2023-03-04 22:28:19 -0500

    updating BarCocktail for methods, including comments for tomorrow

--- app/models/bar_cocktail.rb
+++ app/models/bar_cocktail.rb
@@ -2,13 +2,30 @@ class BarCocktail < ApplicationRecord
   belongs_to :bar
   belongs_to :cocktail
 
-     ## create a custom method here to render something like 
-  ## cocktail_drink.name at bar.name
-  ## so each bar_cocktails has it built in via method
+##! need to review the intialize method 
+  # @@total_count = []
+
+  # ## on initialization 
+  # def initialize
+  #     @@total_count << self
+  # end
+
+
+  # def self.how_many
+  #   @@total_count
+  # end
+
+  def average_rating
+    ## take all reviews associated with this instance
+    ## get the average rating (a few ways to do it)
+  end
 
 
   ###! need to remember how to find the associated method without explicitly spelling it out
 
+     ## create a custom method here to render something like 
+  ## cocktail_drink.name at bar.name
+  ## so each bar_cocktails has it built in via method
   def identity
     "#{self.bar_name} at #{self.drink_name}"
   end
